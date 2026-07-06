bb83e74d0b719409e47c753a3c55da1e4036a754
Iahara <isharaktii@gmail.com>
2026-07-06 06:41:29 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { useSession, signOut } from "next-auth/react";
